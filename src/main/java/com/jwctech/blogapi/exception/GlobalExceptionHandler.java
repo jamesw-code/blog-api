06fa2a7a5f06394c8445ae8ae4c2108afd91ac68
James Wyatt <78618659+Arctic-Gif@users.noreply.github.com>
2022-07-03 08:40:47 -0400

    Handle Specific Exceptions

---
+++ src/main/java/com/jwctech/blogapi/exception/GlobalExceptionHandler.java
@@ -0,0 +1,31 @@
+package com.jwctech.blogapi.exception;
+
+import com.jwctech.blogapi.payload.ErrorDetails;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.context.request.WebRequest;
+
+import java.util.Date;
+
+@ControllerAdvice
+public class GlobalExceptionHandler {
+
+    // Handle specific exceptions
+    @ExceptionHandler
+    public ResponseEntity<ErrorDetails> handleResourceNotFoundException(
+            ResourceNotFoundException exception,
+            WebRequest webRequest){
+        ErrorDetails errorDetails = new ErrorDetails(new Date(), exception.getMessage(), webRequest.getDescription(false));
+        return new ResponseEntity<>(errorDetails, HttpStatus.NOT_FOUND);
+    }
+    @ExceptionHandler
+    public ResponseEntity<ErrorDetails> handleBlogAPIException(
+            BlogAPIException exception,
+            WebRequest webRequest){
+        ErrorDetails errorDetails = new ErrorDetails(new Date(), exception.getMessage(), webRequest.getDescription(false));
+        return new ResponseEntity<>(errorDetails, HttpStatus.BAD_REQUEST);
+    }
+    // Handle Global exceptions
+}
