ab11fd047c6d81fbefb81ec7cd54b0ca2381256a
James Wyatt <78618659+Arctic-Gif@users.noreply.github.com>
2022-07-06 08:57:00 -0400

    Adding Validations to CommentPayload and Adding validation exception handling

--- src/main/java/com/jwctech/blogapi/exception/GlobalExceptionHandler.java
+++ src/main/java/com/jwctech/blogapi/exception/GlobalExceptionHandler.java
@@ -1,32 +1,72 @@
 package com.jwctech.blogapi.exception;
 
 import com.jwctech.blogapi.payload.ErrorDetails;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.method.annotation.MethodArgumentTypeMismatchException;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 import org.springframework.web.context.request.WebRequest;
 
 import java.util.Date;
 
 @ControllerAdvice
-public class GlobalExceptionHandler {
+public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
 
     // Handle specific exceptions
-    @ExceptionHandler
+    @ExceptionHandler(ResourceNotFoundException.class)
     public ResponseEntity<ErrorDetails> handleResourceNotFoundException(
             ResourceNotFoundException exception,
             WebRequest webRequest){
         ErrorDetails errorDetails = new ErrorDetails(new Date(), exception.getMessage(), webRequest.getDescription(false));
         return new ResponseEntity<>(errorDetails, HttpStatus.NOT_FOUND);
     }
-    @ExceptionHandler
+    @ExceptionHandler(BlogAPIException.class)
     public ResponseEntity<ErrorDetails> handleBlogAPIException(
             BlogAPIException exception,
             WebRequest webRequest){
         ErrorDetails errorDetails = new ErrorDetails(new Date(), exception.getMessage(), webRequest.getDescription(false));
         return new ResponseEntity<>(errorDetails, HttpStatus.BAD_REQUEST);
     }
+
+    @ExceptionHandler(MethodArgumentTypeMismatchException.class)
+    public ResponseEntity<ErrorDetails> handleMethodArgumentTypeMismatchException (MethodArgumentTypeMismatchException ex,
+                                                                                WebRequest request) {
+        ErrorDetails errorDetails = new ErrorDetails(new Date(), ex.getMessage(), request.getDescription(false));
+        return new ResponseEntity<>(errorDetails, HttpStatus.BAD_REQUEST);
+
+    }
+
+    @Override
+    protected ResponseEntity<Object> handleMethodArgumentNotValid(MethodArgumentNotValidException ex,
+                                                                  HttpHeaders headers,
+                                                                  HttpStatus status,
+                                                                  WebRequest request) {
+//        Cycle through each error
+//        Map<String, String> errors = new HashMap<>();
+//        ex.getBindingResult().getAllErrors().forEach((error) ->{
+//            String fieldName = ((FieldError)error).getField();
+//            String message = error.getDefaultMessage();
+//            errors.put(fieldName, message);
+//        });
+        // Get field Name from first error
+        String fieldName = ((FieldError)ex.getBindingResult().getAllErrors().get(0)).getField();
+        // Get Error Message from first error
+        String message = ex.getBindingResult().getAllErrors().get(0).getDefaultMessage();
+        // Make Error message
+        String errorMessage = fieldName + ": "+ message;
+        ErrorDetails errorDetails = new ErrorDetails(
+                new Date(),
+                errorMessage,
+                request.getDescription(false));
+
+        return new ResponseEntity<>(errorDetails, HttpStatus.BAD_REQUEST);
+    }
+//      Comment Out till project complete
     // Handle Global exceptions
 //    @ExceptionHandler
 //    public ResponseEntity<ErrorDetails> handleGlobalExceptions(Exception exception, WebRequest webRequest){
--- src/main/java/com/jwctech/blogapi/payload/CommentPayload.java
+++ src/main/java/com/jwctech/blogapi/payload/CommentPayload.java
@@ -2,11 +2,16 @@ package com.jwctech.blogapi.payload;
 
 import lombok.Data;
 
+import javax.validation.constraints.NotEmpty;
+
 @Data
 public class CommentPayload {
 
     private Long id;
+    @NotEmpty
     private String name;
+    @NotEmpty
     private String email;
+    @NotEmpty
     private String body;
 }
